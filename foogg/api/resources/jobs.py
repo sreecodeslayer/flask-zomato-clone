74c02df7d790925e38643994e965d411d593b710
Sreenadh TC <kesav.tc8@gmail.com>
2018-11-18 12:16:14 +0530

    Naive GET api for jobs

--- foogg/api/resources/jobs.py
+++ foogg/api/resources/jobs.py
@@ -1,9 +1,14 @@
 from .base import ManagerResources
+from ...schemas import JobSchema
+from ...models import Jobs
+from ...helpers import paginate
 
 
 class JobsResource(ManagerResources):
     def get(self):
-        pass
+        schema = JobSchema(many=True)
+        jobs = Jobs.objects()
+        return paginate(jobs, schema)
 
 
 class JobResource(ManagerResources):
