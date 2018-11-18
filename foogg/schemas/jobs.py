0bdfb2b29e3c226d9a815677f8823f33b84584f8
Sreenadh TC <kesav.tc8@gmail.com>
2018-11-18 12:36:15 +0530

    Create object post load of request data

--- foogg/schemas/jobs.py
+++ foogg/schemas/jobs.py
@@ -1,23 +1,30 @@
 from datetime import datetime, timedelta
 from foogg.extensions import ma
 from .user import UserSchema
+from .base import ObjectId
+from ..models import Jobs
+
 from marshmallow.exceptions import ValidationError
-PRIORITIES = {
-    0: 'LOW',
-    1: 'MEDIUM',
-    2: 'HIGH'
-}
+from marshmallow import post_load
+
+PRIORITIES = ['low', 'medium', 'high']
 
 
 def validate_choice(value):
-    if value in PRIORITIES.values():
+    if value in PRIORITIES:
         return True
     else:
-        raise ValidationError(f'Priority can be any of {PRIORITIES.values()}')
+        raise ValidationError(
+            f'Priority can be any of {PRIORITIES}')
 
 
 class JobSchema(ma.Schema):
+    id = ObjectId()
     title = ma.String(required=True)
     priority = ma.String(required=True, validate=validate_choice)
     created_on = ma.DateTime(default=datetime.utcnow)
     created_by = ma.Nested(UserSchema)
+
+    @post_load
+    def make_job(self, data):
+        return Jobs(**data)
