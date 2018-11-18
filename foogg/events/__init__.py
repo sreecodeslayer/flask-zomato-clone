3848bf23590f3169ca3b3afff3d9979d8dc9fce6
Sreenadh TC <kesav.tc8@gmail.com>
2018-11-18 21:00:10 +0530

    Add basic events for connect and disconnect

--- foogg/events/__init__.py
+++ foogg/events/__init__.py
@@ -3,7 +3,8 @@ from ..extensions import socketio
 from ..log import logger
 from threading import Lock
 from flask import request
-from flask_socketio import join_room, leave_room, emit
+from flask_jwt_extended import jwt_required
+from flask_socketio import join_room, leave_room, emit, send
 ROOM = 'realtime_updates'
 
 
@@ -11,13 +12,14 @@ ROOM = 'realtime_updates'
 def connected():
     logger.debug('Client [%s] connected for updates' % (request.sid))
     join_room(ROOM)
-    emit('status', {'msg': request.sid +
-                    ' has subscribed to real time updates.'}, room=ROOM)
+    emit(
+        'status', {
+            'msg': request.sid + ' has subscribed to real time updates.'},
+        room=ROOM
+    )
 
 
 @socketio.on('disconnect', namespace='/updates')
 def disconnect():
-    logger.debug('Client [%s] connected for updates' % (request.sid))
+    logger.debug('Client [%s] disconnected from updates' % (request.sid))
     leave_room(ROOM)
-    emit('status', {'msg': request.sid +
-                    ' has subscribed to real time updates.'}, room=ROOM)
