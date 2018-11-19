9b3d58393f561e497253ec9e815fa1b4b4729da1
Sreenadh TC <kesav.tc8@gmail.com>
2018-11-19 10:52:18 +0530

    Add basic UI setup for manager

--- foog_ui/src/App.vue
+++ foog_ui/src/App.vue
@@ -1,36 +1,31 @@
 <template>
  <div id="app" v-if="!$route.meta.public">
-    <!-- Main pages -->
-    <router-view/>
-  </div>
-  <div id="app" v-else style="height='100%'">
-    <!-- Login, Signup, About pages -->
-    <router-view/>
-  </div>
+  <!-- Main pages -->
+  <el-container id="main-container" style="height: calc(92vh);">
+  <nav-bar/>
+    <el-main>
+      <router-view/>
+    </el-main>
+    <el-footer></el-footer>
+  </el-container>
+</div>
+<div id="app" v-else style="height='100%'">
+  <!-- Login, Signup, About pages -->
+  <router-view/>
+</div>
 </template>
 
 <script>
+import Home from '@/views/Home'
+import NavBar from '@/components/NavBar'
 export default {
   name: 'app',
+  components: { NavBar, Home },
   data () {
     return {
+
+      activeIndex: '1'
     }
-  },
-  sockets: {
-    connect () {
-    },
-    disconnect () {
-    },
-    status (data) {
-      this.$notify({
-        title: data.title,
-        message: this.$createElement('i', { style: 'color: teal' }, data.message)
-      })
-    },
-    realtime (data) {
-    }
-  },
-  created () {
   }
 }
 </script>
@@ -38,11 +33,6 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 
 .text-xs {
---
+++ foog_ui/src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-header>
+    <el-menu :default-active="activeIndex"  text-color="grey" router mode="horizontal"  active-text-color="teal">
+      <el-menu-item index="1" :route="{path:'/'}">Home</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">Tasks</template>
+        <el-menu-item index="2-1" :route="{path:'/tasks/new'}">New</el-menu-item>
+        <el-menu-item index="2-2" :route="{path:'/tasks'}">All Tasks</el-menu-item>
+      </el-submenu>
+    </el-menu>
+  </el-header>
+</template>
+<style></style>
+<script>
+export default {
+  name: 'NavBar',
+  data () {
+    return {
+
+      activeIndex: '1'
+    }
+  }
+}
+</script>
