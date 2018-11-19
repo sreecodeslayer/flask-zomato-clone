71546a0d3ecc30d0ef0a1bb784b754ef0fafe8a0
Sreenadh TC <kesav.tc8@gmail.com>
2018-11-19 22:21:35 +0530

    Add background thread to poll queue and push notify the room if queue is not empty

--- foogg/events/__init__.py
+++ foogg/events/__init__.py
@@ -1,11 +1,39 @@
 from flask_socketio import emit, join_room, leave_room, disconnect
-from ..extensions import socketio
+from ..extensions import socketio, rmq
 from ..log import logger
 from threading import Lock
 from flask import request
 from flask_jwt_extended import jwt_required
 from flask_socketio import join_room, leave_room, emit, send
+from threading import Lock
+
 ROOM = 'realtime_updates'
+
+thread = None
+lock = Lock()
+
+
+def background_thread():
+    """Check if the queue is empty here, send broadcast if not"""
+    count = 0
+    while True:
+        socketio.sleep(15)
+        count = rmq.count
+        logger.debug(f'{count} tasks are in queue')
+        if count:
+            socketio.emit(
+                'realtime',
+                {
+                    'message': 'A new task is available'
+                },
+                json=True,
+                namespace='/updates'
+            )
+
+
+with lock:
+    if thread is None:
+        thread = socketio.start_background_task(target=background_thread)
 
 
 @socketio.on('connect', namespace='/updates')
