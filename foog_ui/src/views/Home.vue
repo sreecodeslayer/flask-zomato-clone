f50ef2f9287841dcd74b2ced621161c7dfb51894
Sreenadh TC <kesav.tc8@gmail.com>
2018-11-23 15:54:18 +0530

    Add valet table visualization

--- foog_ui/src/views/Home.vue
+++ foog_ui/src/views/Home.vue
@@ -1,12 +1,57 @@
 <template>
-  <div id="home-wrapper"></div>
+  <div id="home-wrapper">
+    <el-row>
+      <el-col :span="24"></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <el-table :data="tasks.results" style="width: 100%">
+          <el-table-column type="expand">
+            <template slot-scope="scope">
+              <h4>History</h4>
+              <span v-for="his in scope.row.history">
+                <el-tag size="medium" type="success">{{ his.status }}</el-tag>&nbsp;(Updated {{his.made_at | humanizeTime}})
+              </span>
+            </template>
+          </el-table-column>
+          <el-table-column label="Title" prop="title"></el-table-column>
+          <el-table-column label="Current Status" prop="status"></el-table-column>
+          <el-table-column label="Priority">
+            <template slot-scope="scope">
+              <el-tag size="medium" v-if="scope.row.priority === 'low'" type="warning">{{ scope.row.priority }}</el-tag>
+              <el-tag size="medium" v-if="scope.row.priority === 'medium'" type="info">{{ scope.row.priority }}</el-tag>
+              <el-tag size="medium" v-if="scope.row.priority === 'high'" type="danger">{{ scope.row.priority }}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column label="Created on">
+            <template slot-scope="scope">
+              {{scope.row.created_on | calendarTime}}
+            </template>
+          </el-table-column>
+          <el-table-column label="Action">
+            <template slot-scope="scope">
+                <el-button size="small" type="danger" icon="el-icon-delete" circle @click></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
 import '@/plugins/socketio.js'
 export default {
   name: 'home',
-  data () { return {} },
+  data () {
+    return {
+
+      tasks: {
+        pagination: { page: 1, total: 0, totalPages: 0, perPage: 10 },
+        results: []
+      }
+    }
+  },
   sockets: {
     connect () {
     },
@@ -20,6 +65,27 @@ export default {
     },
     realtime (data) {
     }
+  },
+  methods: {
+    fetchMyTasks () {
+      let url = '/api/v1/jobs' +
+        '?page=' + this.tasks.pagination.page +
+        '&perPage=' + this.tasks.pagination.perPage
+
+      this.$http.get(url).then(
+        (response) => {
+          this.tasks.results = response.data.results
+          this.tasks.pagination.totalPages = response.data.total_pages
+          this.tasks.pagination.total = response.data.total
+        },
+        (err) => {
+
+        }
+      )
+    }
+  },
+  created () {
+    this.fetchMyTasks()
   }
 }
 </script>
