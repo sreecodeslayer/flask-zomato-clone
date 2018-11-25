94451db0ca9fd4a520f52f79e1872d407f241a27
Sreenadh TC <kesav.tc8@gmail.com>
2018-11-25 18:39:57 +0530

    Remove port from prod

--- foogg/config.py
+++ foogg/config.py
@@ -21,14 +21,12 @@ class Production(Development):
     DEBUG = False
     SECRET_KEY = "sup3rs3cr3tk3yf0rPr0duct10N"
     HOST = os.getenv('MONGO_HOST', 'localhost')
-    PORT = os.getenv('MONGO_PORT', 27017)
-    DB = os.getenv('MONGO_DB', 'FOOGG')
+    DB = os.getenv('MONGO_DB', 'foogg')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=14)
 
     MONGODB_SETTINGS = [{
         'db': DB,
         'host': HOST,
-        'port': PORT
     }]
 
 
